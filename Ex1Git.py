ad8440056c1d123677b0372f2396cd4d2c70d6dd
Jessie Bakashika <jessie.bakashika@gmail.com>
2021-04-16 15:24:19 +0200

    ajout de DisplayFullName

--- Ex1Git.py
+++ Ex1Git.py
@@ -4,9 +4,12 @@ def DisplayName(_name):
 def DisplayFirstName(_firstname):
 	print(f"Votre nom est {_firstname}")
 
+def DisplayFullName(_name,_firstname):
+	print(f"Vous vous appelez {_firstname} {_name}") if _name != "Bakashika" and _firstname !="Jessie" else print(f"Bonjour votre majesté {_firstname} {_name} ")
 
-name = input("Quel est ton nom de famille. \t")
-firstname = input("Quel est ton prénom. \t")
+name = input("Quel est ton nom de famille? \t")
+firstname = input("Quel est ton prénom? \t")
 
 DisplayName(name)
-DisplayFirstName(firstname)	
+DisplayFirstName(firstname)
+DisplayFullName(name, firstname)
