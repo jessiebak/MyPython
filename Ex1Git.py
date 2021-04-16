699d1b8fcfadc1bdb20a77634733516a38b616aa
Jessie Bakashika <jessie.bakashika@gmail.com>
2021-04-16 15:12:10 +0200

    Ajout de DisplayFirstname

--- Ex1Git.py
+++ Ex1Git.py
@@ -1,8 +1,12 @@
 
 def DisplayName(_name):
 	print(f"Votre nom est {_name}")
+def DisplayFirstName(_firstname):
+	print(f"Votre nom est {_firstname}")
+
 
 name = input("Quel est ton nom de famille. \t")
 firstname = input("Quel est ton prénom. \t")
 
 DisplayName(name)
+DisplayFirstName(firstname)	
